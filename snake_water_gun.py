e3021f38c08304393b2cb81ede49a62253edc6b3
Amit Chaudhary <ak.chaudhary8125@gmail.com>
2026-01-02 18:56:42 +0545

    implementing exit option and fixed bugs

--- snake_water_gun.py
+++ snake_water_gun.py
@@ -9,50 +9,57 @@ import random
 # compter's choice
 computer = random.choice([0,1,-1])
 
-# Taking user's choice and converting it into 0,1,-1
-user_str = input("""Enter 
-s for snake 
-w for water 
-g for gun
-Your choice: """)
+while True:
+    # Taking user's choice and converting it into 0,1,-1
+    user_str = input("""Enter 
+    s for snake 
+    w for water 
+    g for gun
+    e for exit
+    Your choice: """)
 
-user_dict = {"s" : 1 , "w" : -1, "g": 0}
-clean_user = user_str.strip().lower()
-if (clean_user=="s" or clean_user=="w" or clean_user=="g"):
-    user = user_dict[clean_user]
+    user_dict = {"s" : 1 , "w" : -1, "g": 0}
+    clean_user = user_str.strip().lower()
 
-else:
-    print("Choose a valid option!!!")
-    
+    if (clean_user=="s" or clean_user=="w" or clean_user=="g"):
+        user = user_dict[clean_user]
+
+        # Showing user's and computer's choice
+        reverse_dict = {1: "Snake", -1:"Water", 0:"Gun"}
+        print(f"Your Choice: {reverse_dict[user]} \nComputer's Choice: {reverse_dict[computer]}\n")
+
+        # Result
+        if (computer == user):
+            print("Its a Draw!!!\n")
+
+        elif (computer==-1 and user==1):
+            print("You Win!!!\n")
+
+        elif(computer==-1 and user==0):
+            print("Computer Wins!!!\n")
+
+        elif(computer==1 and user==-1):
+            print("Computer Wins!!!\n")
+
+        elif(computer==1 and user==0):
+            print("You Win!!!\n")
+            
+        elif(computer==0 and user==-1):
+            print("You Win!!!\n")
+                
+        elif(computer==0 and user==1):
+            print("Computer Wins!!!\n")
+
+        else: 
+            print("Something went wrong!\n")
+
+    elif (user_str.strip().lower()=="e"):
+        print("Exiting game.....")
+        break
+                
+
+    else:
+            print("Choose a valid option!!!")
+            
 
 
-# Showing user's and computer's choice
-reverse_dict = {1: "Snake", -1:"Water", 0:"Gun"}
-print(f"Your Choice: {reverse_dict[user]} \nComputer's Choice: {reverse_dict[computer]}\n")
-
-# Result
-if (computer == user):
-    print("Its a Draw!!!\n")
-
-else:
-    if (computer==-1 and user==1):
-        print("You Win!!!\n")
-
-    elif(computer==-1 and you==0):
-        print("Computer Wins!!!\n")
-
-    elif(computer==1 and user==-1):
-        print("Computer Wins!!!\n")
-
-    elif(computer==1 and user==0):
-        print("You Win!!!\n")
-    
-    elif(computer==0 and user==-1):
-        print("You Win!!!\n")
-        
-    elif(computer==0 and user==1):
-        print("Computer Wins!!!\n")
-
-    else: 
-        print("Something went wrong!\n")
-        
